1be6a8f365eeeee4a2ce6ea80bab27a4f0e3e5d5
qwerji <bentswanson@gmail.com>
2017-04-29 13:39:18 -0700

    seasonal backgrounds

---
+++ seasons.js
@@ -0,0 +1,27 @@
+const month = `${(new Date()).getMonth()}`
+let season = 'fall'
+switch (month) {
+    case '12':
+    case '1':
+    case '2':
+        season = 'winter'
+        break
+    case '3':
+    case '4':
+    case '5':
+        season = 'spring'
+        break
+    case '6':
+    case '7':
+    case '8':
+        season = 'summer'
+        break
+    case '9':
+    case '10':
+    case '11':
+        season = 'fall'
+        break
+}
+
+document.querySelector('main div.upper').style.background = 
+document.body.style.background = `url(./images/${season}.jpg) no-repeat center center fixed`
